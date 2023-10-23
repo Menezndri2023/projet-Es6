8449a43b7ba1ae7dda16217a6b34cc1722a48578
Menezndri2023 <ndrimanasse@gmail.com>
2023-10-23 13:08:36 +0100

    reste a trouver le mot le plus long

---
+++ algo.js
@@ -0,0 +1,37 @@
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+let obj= {}
+
+for(item of data ){
+    if (!obj[item]){
+        obj[item] = 0;
+    }
+   
+        obj[item]++
+   
+}
+console.log(obj)
+
+let sentence = `short sentence delivers a powerful message with concise and effective language. short sentence is a shanghai-based womenswear brand founded in 2015. the brand pays homage to modern women through a collection of comfortable and timeless essentials. commonplace and banal elements in everyday life inspire short sentence to design understated yet evocative pieces  and pursue life-affirming fashion.`
+
+    // let compt = 0
+    // for(let i = 0 ; i < sentence.length ; i++){
+    //     if(sentence[i] == ' '){
+    //         compt++
+    //     }
+    // }
+    // console.log(compt)
+    // let wordCompt = 0
+    // for(word of sentence){
+    //     if (word[sentence] !== ' '){
+    //         wordCompt++
+    //     }
+        
+    // }
+    // console.log(wordCompt)
+    
+let hightWord = 0
+ for (!)
+
+
+
+
